ea6f204b1e728d6e8ac6f550a0677b01ab6f1625
ShornikovNazar <nazar.shornikov04@gmail.com>
2024-10-27 21:46:08 +0000

    27.10.24-test

--- calc.cpp
+++ calc.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <math.h>
-#include <windows.h>
+//#include <windows.h>
 using namespace std;
 
 int main()
 {
-    SetConsoleOutputCP(1251);
+    //SetConsoleOutputCP(1251);
     int  o;
     double x, y;
     cout << "Введіть числа" << endl;
